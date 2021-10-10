e3cd14578e9af3eece75df780c7be0441e21ba6a
Tobias Falk <tobiasfalk200242@gmail.com>
2021-10-10 14:19:35 +0200

    .

--- build-middleware-Desktop_Qt_5_12_7_GCC_64bit_System-Release/qtcsettings.cmake
+++
@@ -1,8 +0,0 @@
-# This file is managed by Qt Creator, do not edit!
-
-set("CMAKE_BUILD_TYPE" "Release" CACHE "STRING" "" FORCE)
-set("CMAKE_PROJECT_INCLUDE_BEFORE" "/opt/Qt/Tools/QtCreator/share/qtcreator/package-manager/auto-setup.cmake" CACHE "PATH" "" FORCE)
-set("QT_QMAKE_EXECUTABLE" "/usr/bin/qmake-qt5" CACHE "STRING" "" FORCE)
-set("CMAKE_PREFIX_PATH" "/usr" CACHE "STRING" "" FORCE)
-set("CMAKE_C_COMPILER" "/usr/bin/gcc-10" CACHE "STRING" "" FORCE)
-set("CMAKE_CXX_COMPILER" "/usr/bin/g++-10" CACHE "STRING" "" FORCE)
