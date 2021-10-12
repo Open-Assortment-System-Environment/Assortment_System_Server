a03b9eeae14fd22bec848a881b6ebefeab6c93f3
Tobias Falk <tobiasfalk200242@gmail.com>
2021-10-12 07:31:52 +0200

    added json parser funtction

--- middleware/lib/requestParsers/apijson.cpp
+++ middleware/lib/requestParsers/apijson.cpp
@@ -7,34 +7,54 @@ ApiJSON::ApiJSON(QObject* parent)
 
 void ApiJSON::service(stefanfrings::HttpRequest &request, stefanfrings::HttpResponse &response)
 {
+    // request Data
     QByteArray requestData = request.getBody();
-    qDebug() << "Body: " << requestData;
     QByteArray responseData = "";
 
+    // Check Request data
     if(requestData.isEmpty() == true)
     {
         responseData = "No Request";
     }
     else
     {
-        QJsonDocument jsonDocument = QJsonDocument::fromJson(requestData);
-        if(jsonDocument.isObject() == false)
+        // convert to JSONDocument
+        QJsonDocument jsonRequestDocument = QJsonDocument::fromJson(requestData);
+        if(jsonRequestDocument.isObject() == false)
         {
             responseData = "No JSON Request";
         }
         else
         {
+            // Convert to JSON Objekt
+            QJsonObject jsonRequestObject = jsonRequestDocument.object();
 
-            QJsonObject object = jsonDocument.object();
+            // Get and save sesponse
+            QJsonObject jsonLastRequest = jsonRequestObject["request"].toObject();
 
-            QJsonObject JSONrequest = object.value("request").toObject();
-            QJsonValue name = JSONrequest.value("request_type");
-            JSONrequest.insert("request_type", "42ABC42");
-            object.insert("request", JSONrequest);
-            QJsonDocument jsonRes;
-            jsonRes.setObject(object);
-            qDebug() << "RES: " << jsonRes.toJson(QJsonDocument::Compact);
-            responseData = jsonRes.toJson(QJsonDocument::Compact);
+            // Creat response objeckt
+            QJsonObject jsonResult;
+
+            // Check if request is already completed
+            if(jsonLastRequest["completeed"] == false)
+            {
+                // pars request
+                parsJSON(jsonLastRequest, jsonResult);
+            }
+            else
+            {
+                // return exiting result to client
+                jsonResult = jsonRequestObject["result"].toObject();
+            }
+
+            // Creat final Json Objeckt and add request and response
+            QJsonObject jsonResponse;
+            jsonResponse.insert("request", jsonLastRequest);
+            jsonResponse.insert("result", jsonResult);
+
+            // Convert JSON to text
+            QJsonDocument jsonResponseDocument(jsonResponse);
+            responseData = jsonResponseDocument.toJson(QJsonDocument::Compact);
         }
     }
 
@@ -43,4 +63,17 @@ void ApiJSON::service(stefanfrings::HttpRequest &request, stefanfrings::HttpResp
     response.setHeader("Out-Content-Length", responseData.length());
     response.setHeader("In-Content-Length", requestData.length());
     response.write(responseData,true);
+}
+
+void ApiJSON::parsJSON(QJsonObject &request, QJsonObject &result)
+{
+    // temp test code
+    QJsonValue name = request.value("request_type");
+    request["request_type"] = "42ABC42";
+    result.insert("test", 42);
+
+    // pars json and search DB
+
+    // Mark request as completed
+    request["completeed"] = true;
 }
--- middleware/lib/requestParsers/apijson.h
+++ middleware/lib/requestParsers/apijson.h
@@ -8,12 +8,33 @@
 // QtWebApp Includes
 #include "httprequesthandler.h"
 
+///
+/// \brief The ApiJSON class parses and handels the JSON Api requests
+///
 class ApiJSON : public stefanfrings::HttpRequestHandler
 {
     Q_OBJECT
+private:
 public:
+    ///
+    /// \brief ApiJSON the constructor
+    /// \param parent Qobjekt parent
+    ///
     ApiJSON(QObject* parent=0);
+
+    ///
+    /// \brief service ist the function that is called if a request comes in
+    /// \param request the request
+    /// \param response the respons to send
+    ///
     void service(stefanfrings::HttpRequest& request, stefanfrings::HttpResponse& response);
+
+    ///
+    /// \brief parsJSON parses the Json requset
+    /// \param request the request to pars
+    /// \param response the reponse that is created
+    ///
+    void parsJSON(QJsonObject& request, QJsonObject& result);
 };
 
 #endif // APIJSON_H
