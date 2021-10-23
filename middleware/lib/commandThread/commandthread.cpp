d826ba4797763603ba0716e7a369825e0d426d5e
Tobias Falk <tobiasfalk200242@gmail.com>
2021-10-23 23:14:16 +0200

    added comments

--- middleware/lib/commandThread/commandthread.cpp
+++ middleware/lib/commandThread/commandthread.cpp
@@ -16,18 +16,18 @@ void CommandThread::run()
     QTextStream in(stdin);
     while(true)
     {
-        QString value = in.readLine();
-        if(value.length() != 0)
+        QString value = in.readLine(); // the read in value
+        if(value.length() != 0) // cheches if sonthing was entered or not
         {
-            QString comm = value;
-            QString par = "";
+            QString comm = value; // the atcual command
+            QString par = ""; // the parameters
             if(value.indexOf(":") > 0)
             {
-                QStringList commANDpar = value.split(':');
-                comm = commANDpar[0];
-                par = commANDpar[1];
+                QStringList commANDpar = value.split(':'); // Splits the command from the parameters
+                comm = commANDpar[0]; // writs the command in to the command variable
+                par = commANDpar[1]; //writs the parameters in to the paramters variable
             }
-            parsCommand(comm, par);
+            parsCommand(comm, par); // runs the pars function
         }
     }
 }
@@ -35,8 +35,8 @@ void CommandThread::run()
 void CommandThread::initCommandMap()
 {
     // no 0
-    commandMap->insert("stop", 1);
-    commandMap->insert("help", 2);
+    commandMap->insert("stop", 1); // writs the stop command in to the command Map
+    commandMap->insert("help", 2); // writs the help command in to the command Map
 }
 
 void CommandThread::parsCommand(QString comm, QString par, bool help)
@@ -44,15 +44,15 @@ void CommandThread::parsCommand(QString comm, QString par, bool help)
     switch (commandMap->value(comm))
     {
         case 0: //when the Command is not known
-            unknownC0();
+            unknownC0(); // runs the unknwn command function
         break;
 
         case 1: //stop command
-            stopC1(par);
+            stopC1(par); // runs the stop command function
         break;
 
         case 2: //help command
-            if(!help)
+            if(!help) // runs the help command function if the help variable not true is
             {
                 helpC2(par);
             }
@@ -62,7 +62,9 @@ void CommandThread::parsCommand(QString comm, QString par, bool help)
 
 void CommandThread::unknownC0()
 {
-    qInfo().noquote() << "Unknown command";
+    qInfo().noquote() << "Unknown command, type help to see all posible commands"; // prints the Unknown Command function
+}
+
 void CommandThread::stopC1(QString parameters)
 {
     if(parameters == "help") // checks if the command help is selekted and if yes than prints the help message
