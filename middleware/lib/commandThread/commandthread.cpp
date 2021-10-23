d02e023c7956c95816d5dd8d25adfcf414b253f6
Tobias Falk <tobiasfalk200242@gmail.com>
2021-10-23 23:13:52 +0200

    moved commands to one .cpp file

--- middleware/lib/commandThread/commandthread.cpp
+++ middleware/lib/commandThread/commandthread.cpp
@@ -63,4 +63,46 @@ void CommandThread::parsCommand(QString comm, QString par, bool help)
 void CommandThread::unknownC0()
 {
     qInfo().noquote() << "Unknown command";
+void CommandThread::stopC1(QString parameters)
+{
+    if(parameters == "help") // checks if the command help is selekted and if yes than prints the help message
+    {
+        qInfo().noquote() << helpStopC1;
+    }
+    else // if not than it stops the Program
+    {
+        delete commandMap;
+        delete staticFileController;
+        qInfo().noquote() << "STOPING APP";
+        this->deleteLater();
+        APP->exit(0);
+    }
+}
+
+void CommandThread::helpC2(QString parameters)
+{
+    if(parameters == "help") // checks if the command help is selekted and if yes than prints the help message
+    {
+        qInfo().noquote() << "Command: " << "help";
+        qInfo().noquote() << "id: " << "2";
+        qInfo().noquote() << helpHelpC2;
+    }
+    else if(parameters.length() > 0 && commandMap->value(parameters) != 0) // displays all helps of all commands
+    {
+        qInfo().noquote() << "Command: " << parameters; // displays the Command name
+        qInfo().noquote() << "id: " << commandMap->value(parameters); // displays the Command id
+        parsCommand(parameters, "help", true); // displays the Command help
+    }
+    else // displays all helps of of the specified commadn in the parameter commands
+    {
+        QList<QString> comKeys = commandMap->keys();
+        foreach(QString key, comKeys)
+        {
+            qInfo().noquote() << "Command: " << key; // displays the Command name
+            qInfo().noquote() << "id: " << commandMap->value(key); // displays the Command id
+            parsCommand(key, "help", true); // displays the Command help
+            qInfo().noquote() << "";
+
+        }
+    }
 }
