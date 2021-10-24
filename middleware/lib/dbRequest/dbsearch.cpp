0403ecc52a5644852a54fa808db238769b385709
Tobias Falk <tobiasfalk200242@gmail.com>
2021-10-24 15:03:03 +0200

    building DBSearch Interface

--- middleware/lib/dbRequest/dbsearch.cpp
+++ middleware/lib/dbRequest/dbsearch.cpp
@@ -1,5 +1,25 @@
 #include "dbsearch.h"
 
+QJsonObject *DBSearch::getReqest() const
+{
+    return reqest;
+}
+
+void DBSearch::setReqest(QJsonObject *newReqest)
+{
+    reqest = newReqest;
+}
+
+QJsonObject *DBSearch::getResult() const
+{
+    return result;
+}
+
+void DBSearch::setResult(QJsonObject *newResult)
+{
+    result = newResult;
+}
+
 DBSearch::DBSearch(QSqlDatabase *DB, QObject *parent)
 {
     db = DB;
--- middleware/lib/dbRequest/dbsearch.h
+++ middleware/lib/dbRequest/dbsearch.h
@@ -33,10 +33,47 @@ private:
     ///
     QJsonObject *result;
 public:
+    ///
+    /// \brief DBSearch the Constructor
+    /// \param DB the DBConnection to be used
+    /// \param parent qt parent Object
+    ///
     explicit DBSearch(QSqlDatabase *DB ,QObject *parent = nullptr);
+    ///
+    /// \brief DBSearch the Constructor
+    /// \param DB the DBConnection to be used
+    /// \param Reqest the pointer to the Request Object
+    /// \param Result the pointer to the Result Object
+    /// \param parent qt parent Object
+    ///
     explicit DBSearch(QSqlDatabase *DB, QJsonObject *Reqest, QJsonObject *Result, QObject *parent = nullptr);
 
+    ///
+    /// \brief searchStart starts the search thrugh the DB
+    ///
     void searchStart();
+
+    ///
+    /// \brief getReqest returns the the pointer to the Reqest Object
+    /// \return the Reqest Object pointer
+    ///
+    QJsonObject *getReqest() const;
+    ///
+    /// \brief setReqest sets the Request Object pointer
+    /// \param newReqest the new Request Pointer
+    ///
+    void setReqest(QJsonObject *newReqest);
+
+    ///
+    /// \brief getResult returns the the pointer to the Result Object
+    /// \return the Result Object pointer
+    ///
+    QJsonObject *getResult() const;
+    ///
+    /// \brief setResult sets the Result Object pointer
+    /// \param newResult the new Result Pointer
+    ///
+    void setResult(QJsonObject *newResult);
 
 signals:
 
